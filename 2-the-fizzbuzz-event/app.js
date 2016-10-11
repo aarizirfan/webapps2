71fa282f8272a5c6346ac0115f9ce802c30635ee
aarizirfan <aairfan2019@mirmanmustangs.org>
2016-10-11 12:52:27 -0700

    working 2

--- 2-the-fizzbuzz-event/app.js
+++ 2-the-fizzbuzz-event/app.js
@@ -1,6 +1,23 @@
 // Your Code Here:
-function fizzBuzz(n) {
-  // Implement FizzBuzz
+var number = Math.floor((Math.random() * 100) + 1);
+
+function createCard(number){
+  var suite;
+  var value;
+  switch(number%4){
+    case 0:
+      suite="Spades";
+    case 1:
+      suite="Hearts";
+    case 2:
+      suite="Clubs";
+    case 3:
+      suite="Diamonds";
+  }
+  switch((number%4)%13){
+    
+  }
+
 }
 
 // Any other functions your app requires
